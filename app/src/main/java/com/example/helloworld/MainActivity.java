e2590a7170865cbb771dda91eba720b5747f5316
Teddy <chankarfu@yahoo.com>
2026-04-23 06:27:15 +0000

    Step 4: Add interactive button with click counter
    Signed-off-by: hkteddychan <hkteddychan@users.noreply.github.com>

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.helloworld;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 public class MainActivity extends Activity {
+    private int clickCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello World!");
-        setContentView(tv);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        final TextView tv = (TextView) findViewById(R.id.textView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickCount++;
+                tv.setText("Clicked " + clickCount + " times");
+            }
+        });
     }
 }
